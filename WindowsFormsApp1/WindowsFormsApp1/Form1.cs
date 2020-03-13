9f1dffbb3621f89db7824e6182b1ff32c747f535
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2020-03-13 17:29:33 -0500

    Add files via upload

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,7 +25,7 @@ namespace WindowsFormsApp1
             if (comboBox1.SelectedItem != null)
             {
                 GlobalVariables.Pathologists = int.Parse(comboBox1.SelectedItem.ToString());
-                MessageBox.Show(GlobalVariables.Pathologists.ToString()); //just for testing
+                //MessageBox.Show(GlobalVariables.Pathologists.ToString()); //just for testing
             }
         }
 
@@ -35,7 +35,8 @@ namespace WindowsFormsApp1
             if (comboBox2.SelectedItem != null)
             {
                 GlobalVariables.SheetNumber = int.Parse(comboBox2.SelectedItem.ToString());
-                MessageBox.Show(GlobalVariables.SheetNumber.ToString()); //just for testing
+                //MessageBox.Show(GlobalVariables.SheetNumber.ToString()); //just for testing
+
             }
         }
 
@@ -51,26 +52,103 @@ namespace WindowsFormsApp1
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = ofd.SafeFileName;
+                GlobalVariables.FileName = ofd.SafeFileName;
                 GlobalVariables.FileLocation = ofd.FileName;
-                MessageBox.Show(GlobalVariables.FileLocation); //just for testing
+                //MessageBox.Show(GlobalVariables.FileLocation); //just for testing
             }
         }
 
         //button2 is submit
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            Excel excel = new Excel(GlobalVariables.FileLocation, GlobalVariables.SheetNumber);
+
+            int sheet = GlobalVariables.SheetNumber;
+            int c = excel.wb.Worksheets[sheet].UsedRange.Columns.Count;
+            int r = excel.wb.Worksheets[sheet].UsedRange.Rows.Count;
+
+            string[,] excelData = new string[r, c];
+
+            for (int row = 1; row <= r; row++)
+            {
+                for (int col = 1; col <= c; col++)
+                {
+                    excelData[row - 1, col - 1] = Convert.ToString(excel.wb.Worksheets[sheet].Cells[row, col].Value2);
+                    //MessageBox.Show(excelData[row - 1, col - 1]);
+                }
+            }
+
+            // example
+            previewImported(excelData);
+
+            //MessageBox.Show(excel.ReadCell(0, 0));
+            //MessageBox.Show("Columns: " + c + "\n" + "Rows: " + r);
+            //sol 
+
+            CloseFile(excel.wb);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //previewImported(excelData);
+        }
+
+        public void CloseFile(Workbook wb)
+        {
+            wb.Close(0);
+        }
+
+        public void previewImported(string[,] excelData)
+        {
+            int height = excelData.GetLength(0);
+            int width = excelData.GetLength(1);
+
+            this.dataGridView1.ColumnCount = width;
+
+            for (int r = 0; r < height; r++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView1);
+
+                for (int c = 0; c < width; c++)
+                {
+                    row.Cells[c].Value = excelData[r, c];
+                }
+
+                this.dataGridView1.Rows.Add(row);
+            }
+        }
+
     }
 
     class Excel
     {
-        RowCount = m_ActiveWorkSheet.UsedRange.Rows.Count;
+        string path = GlobalVariables.FileLocation;
+        _Application excel = new _Excel.Application();
+        public Workbook wb;
+        public Worksheet ws;
+
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+
+            if (ws.Cells[i, j].Value2 != null) return Convert.ToString(ws.Cells[i, j].Value2);
+            else return "";
+        }
     }
 }
 
 public static class GlobalVariables
 {
+    public static String FileName;
     public static String FileLocation;
     public static int Pathologists;
     public static int SheetNumber;
--- WindowsFormsApp1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,19 +37,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(11, 12);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 102;
             this.dataGridView1.RowTemplate.Height = 40;
-            this.dataGridView1.Size = new System.Drawing.Size(1517, 911);
+            this.dataGridView1.Size = new System.Drawing.Size(1518, 911);
             this.dataGridView1.TabIndex = 0;
             // 
             // comboBox1
@@ -72,8 +70,7 @@
             "13",
             "14",
             "15"});
-            this.comboBox1.Location = new System.Drawing.Point(400, 963);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(399, 963);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(161, 39);
             this.comboBox1.TabIndex = 1;
@@ -82,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 971);
+            this.label1.Location = new System.Drawing.Point(46, 970);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(310, 32);
             this.label1.TabIndex = 2;
@@ -91,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 1054);
+            this.label2.Location = new System.Drawing.Point(46, 1053);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(293, 32);
             this.label2.TabIndex = 3;
@@ -100,9 +97,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 1130);
+            this.label3.Location = new System.Drawing.Point(46, 1131);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 32);
+            this.label3.Size = new System.Drawing.Size(197, 32);
             this.label3.TabIndex = 4;
             this.label3.Text = "Sheet";
             // 
@@ -118,8 +115,7 @@
             "5",
             "6",
             "7"});
-            this.comboBox2.Location = new System.Drawing.Point(400, 1123);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Location = new System.Drawing.Point(399, 1124);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(161, 39);
             this.comboBox2.TabIndex = 5;
@@ -128,10 +124,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-            this.button1.Location = new System.Drawing.Point(872, 1035);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(871, 1034);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 60);
+            this.button1.Size = new System.Drawing.Size(161, 59);
             this.button1.TabIndex = 6;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = true;
@@ -139,41 +134,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(400, 1047);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(399, 1047);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(444, 38);
+            this.textBox1.Size = new System.Drawing.Size(445, 38);
             this.textBox1.TabIndex = 7;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(53, 1216);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(52, 1215);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(507, 81);
+            this.button2.Size = new System.Drawing.Size(508, 81);
             this.button2.TabIndex = 8;
             this.button2.Text = "Submit";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-            this.button3.Location = new System.Drawing.Point(1188, 963);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(221, 60);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Preview Slected";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1541, 1312);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1542, 1317);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -183,7 +163,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Submit";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -203,7 +182,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
